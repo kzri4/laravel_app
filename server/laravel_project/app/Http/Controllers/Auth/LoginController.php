4824ce88bb48b4d743af1ad648621185117fa014
kzri4 <kazu4126ab@gmail.com>
2021-04-07 07:58:08 +0900

    ログイン機能まで作成

--- server/laravel_project/app/Http/Controllers/Auth/LoginController.php
+++ server/laravel_project/app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
@@ -20,12 +21,15 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
+    protected $maxAttempt = 5;
+    protected $decayMinutes = 1;
+
     /**
      * Where to redirect users after login.
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
      * Create a new controller instance.
--- server/laravel_project/database/migrations/2021_04_02_071325_create_articles_table.php
+++ server/laravel_project/database/migrations/2021_04_02_071325_create_articles_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateArticlesTable extends Migration
 {
@@ -19,6 +19,7 @@ class CreateArticlesTable extends Migration
             $table->text('body');
             $table->bigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
         });
     }
 
